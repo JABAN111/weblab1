15def76302609941d3e5988e1cbd5d160427edff
Jaban111 <368601@edu.itmo.ru>
2023-09-25 23:16:52 +0300

    da

---
+++ js/script.js
@@ -0,0 +1,50 @@
+// let x,y,R;
+
+const x = document.querySelectorAll(".x")
+console.log(x)
+// window.addEventListener('click',function (event) {
+//     document.getElementById("submit").onclick
+//     {
+//         if(validateX()) {
+//             alert("X а");
+//             return true;
+//         }
+//         else{
+//             return false;
+//         }
+//         if(validateY()){
+//             alert("Y а");
+//             return true;
+//         }else{
+//             return false;
+//         }
+//     }
+//
+
+// });
+// function validateY(){
+//     try {
+//         y = document.getElementById("yValue").value;
+//         if(y === undefined){
+//             alert("lol undefined");
+//             return false;
+//         }
+//         else if(y < -3 || y>5)
+//             return true;
+//     }catch (err){
+//         alert("CHOOOSE Y NIGGER");
+//     }
+// }
+//
+//
+// function validateX(){
+//     try {
+//         x = document.querySelector("input[type = radio]:checked").value
+//         Console.log("x is correct");
+//         return true;
+//     }catch (err){
+//         alert(err)
+//         // alert("u should choose x");//временно)
+//         return false;
+//     }
+// }
---
+++ js/test.js
@@ -0,0 +1,99 @@
+class Car{
+    constructor(name) {
+        this.name = name;
+    }
+    drive(x){
+        return x + " is driving";
+    }
+    moo(){return "moo" || 5;}
+}
+lambert = "chort eban"
+{
+    gay = "chmo"
+    let naturlich = "ne cmo"
+}
+var joo = function (){
+    return window.open("http://google.com","full")
+}
+function fi(callback){
+    setTimeout()console.log("done");
+    callback(),
+        100
+}
+var foo = function (arg1,arg2){
+    return arg1+arg2+joo();
+}
+
+let jon = new Object();
+jon.make = "Mom and Dad";
+jon.model = "s probegom"
+jon.year = 2004;
+console.log(jon.color)
+let car = new Car("jo");
+console.log(car.drive(21));
+function jo(){
+    alert(!22);
+}
+
+let data = document.getElementById("submit");
+
+
+// window.addEventListener('keydown', function (event) {
+//     // используем объект события, чтобы получить информацию о нажатой клавише
+//     alert(`Вы нажали на кнопку: ${event.altKey}`)
+//     console.log("data>");
+//     document.title = "chue, we're home";
+//     console.log(document.title);
+// })
+
+
+oncluck()
+{
+    function dsa(x){
+        return x**2
+    }
+}
+// alert((Math.random()));
+// window.addEventListener('click',function ())
+// window.addEventListener('click',function (event) {
+//     document.getElementById("submit").onclick;
+//     {
+//     if (document.getElementById("yValue") > 5 || document.getElementById("yValue") < -3) {
+//         alert("ПАШОВ РНАВЗЪЩЛ");
+//     }
+//
+//     }
+// })
+document.getElementById("submit").onclick = function() {
+    let yValue = parseFloat(document.getElementById("yValue").value);
+
+    if (yValue > 5 || yValue < -3) {
+        alert("Invalid y value");//временно алерт, дальше взаимодействие через css
+        return false;
+    }
+}
+
+
+window.onload = function (){
+    function setOnClick(element){
+        r = this.value;
+        buttons.forEach()
+    }
+}
+function validateY(yArg){
+    if(yArg < 5 && yArg > -3){
+        return true;
+    }
+    return alert("Невалидное значение y!")
+}
+let inputField = document.getElementById("uzzz");
+
+let userInput = inputField.value;
+if (userInput === "") {
+    alert("Пожалуйста, введите данные в текстовое поле.");
+} else {
+    alert("Вы ввели данные: " + userInput);
+}
+function totalValidation(){
+
+}
