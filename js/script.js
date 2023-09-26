8104e2bf02371098db11c60cf6b0321f6be1400e
Jaban111 <368601@edu.itmo.ru>
2023-09-26 16:11:43 +0300

    da

--- js/script.js
+++ js/script.js
@@ -1,50 +1,99 @@
-// let x,y,R;
+//will be true, when user will choose valid data for them
+let xValid = false;
+let yValid = false;
+let RValid = false;
+// document.addEventListener('DOMContentLoaded', function () {
+//Hooking the value of the input
+const xRadioButtons = document.querySelectorAll(".x");
+const Y = document.getElementById("yValue");
+const RButtons = document.querySelectorAll(".R")
+const submitButton = document.getElementById("submit");
+//add event for every x-button
+for (const element of xRadioButtons) {
+    element.addEventListener('change', isClickedX);
+}
+//add event for every R-selector-button
+for (const element of RButtons) {
+    element.addEventListener('change', isSelectedR);
+}
+Y.addEventListener('input', isValidY);
 
-const x = document.querySelectorAll(".x")
-console.log(x)
-// window.addEventListener('click',function (event) {
-//     document.getElementById("submit").onclick
-//     {
-//         if(validateX()) {
-//             alert("X а");
-//             return true;
-//         }
-//         else{
-//             return false;
-//         }
-//         if(validateY()){
-//             alert("Y а");
-//             return true;
-//         }else{
-//             return false;
-//         }
+function checkEverythingIsValid(){
+    if(xValid && yValid && RValid){
+        alert("ВСЕ ВАЛИДНО");
+        submitButton.disabled = false;
+    }
+    console.log("Валидация не пройдена:" +
+        "xValid=" + xValid + " " +
+        "yValid=" + yValid + " "+
+        "RValid=" + RValid);
+}
+
+
+// window.onload = () => {
+//     const btn = document.getElementById("submit");
+//     btn.setAttribute('disable', '')
+// }
+function isValidY(event) {
+    let y = event.target.value;
+    console.log("someData" + event.target.textContent)
+    console.log("Введенное значение y: " + event.target.value + " " +
+        "тип введеного значения: " + typeof(event.target.value));
+    if(isNaN(y) || y.trim() === ""){
+        yValid = false;
+        checkEverythingIsValid();
+        return false;
+    }else if(y >= -3 && y <= 5){
+        console.log("Введеное значение " + y + " подходит");
+        yValid = true;
+        checkEverythingIsValid();
+        return true;
+    }
+    else{
+        // event.target.setCustomValidity("Значение y должно быть в диапазоне от -3 до 5," +
+        //     "ваше значение: " + y + " если nan: " + event.target.value);
+        yValid = false;
+        checkEverythingIsValid();
+        return false;
+    }
+}
+function isClickedX(event) {
+    xValid = true;
+    checkEverythingIsValid();
+    return true;
+}
+function isSelectedR(event){
+    let R = event.target.value;
+    if(R !== undefined && R !== ""){
+        RValid = true;
+        checkEverythingIsValid();
+    }
+    console.log(R + "- this must be value of R");
+}
+// console.log(xValid);
+// function isClickedX(event) {
+//     let x = event.target.value;
+//     if(!isNan(x) && parseInt(x, 10) === 3){
+//         console.log(x);
 //     }
-//
-
-// });
-// function validateY(){
-//     try {
-//         y = document.getElementById("yValue").value;
-//         if(y === undefined){
-//             alert("lol undefined");
-//             return false;
-//         }
-//         else if(y < -3 || y>5)
-//             return true;
-//     }catch (err){
-//         alert("CHOOOSE Y NIGGER");
+//     if(x !== undefined) {
+//         console.log("change " + event.target.value);
+//         return true;
+//     }
+//     else{
+//         event.preventDefault()
 //     }
 // }
+
+// document.getElementById("submit").addEventListener('click',function (event) {
+//     alert("why")
+//     event.preventDefault();
+//     alert("NO U WON'T")
+// })
+
+
+
 //
-//
-// function validateX(){
-//     try {
-//         x = document.querySelector("input[type = radio]:checked").value
-//         Console.log("x is correct");
-//         return true;
-//     }catch (err){
-//         alert(err)
-//         // alert("u should choose x");//временно)
-//         return false;
-//     }
+// if (isValidY() || isClickedX()) {
+//     console.log("someone good");
 // }
